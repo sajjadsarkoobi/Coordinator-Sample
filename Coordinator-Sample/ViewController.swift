1c4cb1ec1cf57624c5f6ed1f6d7f2e65e3d95537
ssarkoobi@planatechnologies.com <sajjadsarkoobi@gmail.com>
2022-01-30 12:14:06 +0300

    Initial Commit

---
+++ Coordinator-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinator-Sample
+//
+//  Created by Sajjad Sarkoobi on 1/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
