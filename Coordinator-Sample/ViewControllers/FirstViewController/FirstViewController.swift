27626abab011e012cabdeb51fc6c01df7471e6d0
ssarkoobi@planatechnologies.com <sajjadsarkoobi@gmail.com>
2022-01-30 19:08:14 +0300

    Initial Project

---
+++ Coordinator-Sample/ViewControllers/FirstViewController/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  Coordinator-Sample
+//
+//  Created by Sajjad Sarkoobi on 1/30/22.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
